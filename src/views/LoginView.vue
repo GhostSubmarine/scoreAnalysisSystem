7b961061457cf03092b60163ed6f284bddc12a54
GhostSubmarine <35192444+GhostSubmarine@users.noreply.github.com>
2024-04-16 17:25:28 +0800

    初始化项目

---
+++ src/views/LoginView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>dataAnalysisSystem</div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/views/layout/IndexView.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-container>
+    <el-aside width="200px">
+      <el-menu
+        active-text-color="#ffd04b"
+        background-color="#545c64"
+        class="el-menu-vertical-demo"
+        default-active="2"
+        text-color="#fff"
+        @open="handleOpen"
+        @close="handleClose"
+      >
+        <el-menu-item index="1">
+          <router-link>信息管理</router-link>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <router-link>成绩操作</router-link>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <router-link>数据分析</router-link>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
+    <RouterView />
+  </el-container>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<style lang="scss" scoped>
+</style>
